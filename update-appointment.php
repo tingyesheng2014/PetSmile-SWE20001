d7463c76a4d26277ede35f088294f905f1a674eb
tpk1120 <145960225+tpk1120@users.noreply.github.com>
2023-10-10 02:29:29 +0800

    Update update-appointment.php

--- update-appointment.php
+++ update-appointment.php
@@ -6,106 +6,30 @@ if (!isset($_SESSION['Cust_ID'])) {
     exit;
 }
 
-if (!isset($_GET['appointmentID']) || !isset($_GET['appointmentType'])) {
-    header("Location: bookinghistory.php");
-    exit;
-}
+if (isset($_POST['updateAppointment'])) {
+    $appointmentID = $_POST['appointmentID'];
+    $appointmentType = $_POST['appointmentType'];
 
-$appointmentID = $_GET['appointmentID'];
-$appointmentType = $_GET['appointmentType'];
+    $con = mysqli_connect('localhost', 'root', '', 'petsmile');
 
-$con = mysqli_connect('localhost', 'root', '', 'petsmile');
-
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$tableName = "";
-$columnName = "";
-
-if ($appointmentType === "Pet Grooming") {
-    $tableName = "groomingAppt";
-    $columnName = "GAppt_ID";
-} elseif ($appointmentType === "Pet Boarding") {
-    $tableName = "boardingAppt";
-    $columnName = "BAppt_ID";
-} else {
-    header("Location: bookinghistory.php");
-    exit;
-}
-
-$selectQuery = "SELECT * FROM $tableName WHERE $columnName = '$appointmentID'";
-$result = mysqli_query($con, $selectQuery);
-
-if (!$result || mysqli_num_rows($result) === 0) {
-    header("Location: bookinghistory.php");
-    exit;
-}
-
-$appointmentData = mysqli_fetch_assoc($result);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $selectedPetName = mysqli_real_escape_string($con, $_POST['petName']);
-    $selectedStaffName = mysqli_real_escape_string($con, $_POST['staffName']);
-
-    $Cust_ID = $_SESSION['Cust_ID'];
-    $selectPetIDQuery = "SELECT Pet_ID FROM pet WHERE Cust_ID='$Cust_ID' AND Pet_Name = '$selectedPetName'";
-    $petIDResult = mysqli_query($con, $selectPetIDQuery);
-
-    if ($petIDResult && mysqli_num_rows($petIDResult) > 0) {
-        $petIDData = mysqli_fetch_assoc($petIDResult);
-        $selectedPetID = $petIDData['Pet_ID'];
-    } else {
-        $_SESSION['error_message'] = "Pet not found.";
-        header("Location: edit-appointment.php?appointmentID=$appointmentID&appointmentType=$appointmentType");
-        exit;
-    }
-
-    $selectStaffIDQuery = "SELECT Staff_ID FROM staff WHERE Last_Name = '$selectedStaffName'";
-    $staffIDResult = mysqli_query($con, $selectStaffIDQuery);
-
-    if ($staffIDResult && mysqli_num_rows($staffIDResult) > 0) {
-        $staffIDData = mysqli_fetch_assoc($staffIDResult);
-        $selectedStaffID = $staffIDData['Staff_ID'];
-    } else {
-        $_SESSION['error_message'] = "Staff not found.";
-        header("Location: edit-appointment.php?appointmentID=$appointmentID&appointmentType=$appointmentType");
-        exit;
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
     if ($appointmentType === "Pet Grooming") {
-        // Handle grooming appointments
-        $newDate = mysqli_real_escape_string($con, $_POST['new_date']);
-        $newTime = mysqli_real_escape_string($con, $_POST['new_time']);
+        $newDate = $_POST['new_date'];
+        $newTime = $_POST['new_time'];
 
-        if (!validateDate($newDate) || !validateTime($newTime)) {
-            $_SESSION['error_message'] = "Invalid date or time format.";
-            header("Location: edit-appointment.php?appointmentID=$appointmentID&appointmentType=$appointmentType");
-            exit;
-        }
-
-        $updateQuery = "UPDATE groomingAppt SET GAppt_Date = ?, GAppt_Time = ?, Pet_ID = ?, Staff_ID = ? WHERE GAppt_ID = ?";
+        $updateQuery = "UPDATE groomingAppt SET GAppt_Date = ?, GAppt_Time = ? WHERE GAppt_ID = ?";
         $stmt = mysqli_prepare($con, $updateQuery);
-        mysqli_stmt_bind_param($stmt, 'ssiii', $newDate, $newTime, $selectedPetID, $selectedStaffID, $appointmentID);
+        mysqli_stmt_bind_param($stmt, 'ssi', $newDate, $newTime, $appointmentID);
     } elseif ($appointmentType === "Pet Boarding") {
-        // Handle boarding appointments
-        $newStartDate = mysqli_real_escape_string($con, $_POST['start_date']);
-        $newEndDate = mysqli_real_escape_string($con, $_POST['end_date']);
+        $newStartDate = $_POST['start_date'];
+        $newEndDate = $_POST['end_date'];
 
-        if (!validateDate($newStartDate) || !validateDate($newEndDate)) {
-            $_SESSION['error_message'] = "Invalid start date or end date format.";
-            header("Location: edit-appointment.php?appointmentID=$appointmentID&appointmentType=$appointmentType");
-            exit;
-        }
-
-        $updateQuery = "UPDATE boardingAppt SET BAppt_StartDate = ?, BAppt_EndDate = ?, Pet_ID = ?, Staff_ID = ? WHERE BAppt_ID = ?";
+        $updateQuery = "UPDATE boardingAppt SET BAppt_StartDate = ?, BAppt_EndDate = ? WHERE BAppt_ID = ?";
         $stmt = mysqli_prepare($con, $updateQuery);
-        mysqli_stmt_bind_param($stmt, 'ssiii', $newStartDate, $newEndDate, $selectedPetID, $selectedStaffID, $appointmentID);
-    } else {
-        // Handle other appointment types or invalid types
-        $_SESSION['error_message'] = "Invalid appointment type.";
-        header("Location: bookinghistory.php");
-        exit;
+        mysqli_stmt_bind_param($stmt, 'ssi', $newStartDate, $newEndDate, $appointmentID);
     }
 
     if (mysqli_stmt_execute($stmt)) {
@@ -113,8 +37,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: bookinghistory.php");
         exit;
     } else {
-        $_SESSION['error_message'] = "Failed to update the appointment. Please try again later.";
+        $_SESSION['error_message'] = "Failed to update the appointment: " . mysqli_error($con);
+        header("Location: bookinghistory.php");
+        exit;
     }
+} else {
+    header("Location: bookinghistory.php");
+    exit;
 }
-
 ?>
