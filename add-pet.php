c8b1fc729263768d8b5c862cc738c2387d098fb1
tpk1120 <145960225+tpk1120@users.noreply.github.com>
2023-10-27 17:44:21 +0800

    Update add-pet.php

--- add-pet.php
+++ add-pet.php
@@ -7,12 +7,11 @@ include 'header.php';
 
 if (isset($_POST['addPet'])) {
     $Cust_ID = $_SESSION['Cust_ID'];
-    $Pet_ID = mysqli_real_escape_string($con, $_POST['petid']);
     $Pet_Name = mysqli_real_escape_string($con, $_POST['petname']);
     $Pet_Sex = mysqli_real_escape_string($con, $_POST['petsex']);
     $Pet_Breed = mysqli_real_escape_string($con, $_POST['petbreed']);
 
-    $insertPetQuery = "INSERT INTO pet (Cust_ID, Pet_ID, Pet_Name, Pet_Sex, Pet_Breed) VALUES ('$Cust_ID', '$Pet_ID', '$Pet_Name', '$Pet_Sex', '$Pet_Breed')";
+    $insertPetQuery = "INSERT INTO pet (Cust_ID, Pet_Name, Pet_Sex, Pet_Breed) VALUES ('$Cust_ID', '$Pet_Name', '$Pet_Sex', '$Pet_Breed')";
 
     if (mysqli_query($con, $insertPetQuery)) {
         echo '<div class="alert alert-success"><strong>New pet added successfully!</strong></div>';
@@ -23,36 +22,40 @@ if (isset($_POST['addPet'])) {
 ?>
 
     <!-- Carousel Start -->
-    <form method="POST" action="add-pet.php">
-        <h2>Add New Pet</h2>
-
-        <!-- Pet details -->
-        <div>
-            <label for="petid">Pet ID:</label>
-            <input type="text" name="petid" id="petid" required>
+    <body>
+        <div class="container text-center mt-5">
+            <h2 class="display-4 m-0">Add <span class="text-primary">New Pet</span></h2>
+            <h3>Add Pet</h3>
         </div>
 
-        <div>
-            <label for="petname">Pet Name:</label>
-            <input type="text" name="petname" id="petname" required>
+        <div class="container">
+            <form method="POST" action="add-pet.php">
+                <!-- Pet details -->
+                <div>
+                    <label for="petname">Pet Name:</label>
+                    <input type="text" name="petname" id="petname" required>
+                </div>
+
+                <div>
+                    <label for="petsex">Pet Sex:</label>
+                    <input type="text" name="petsex" id="petsex" required>
+                </div>
+
+                <div>
+                    <label for="petbreed">Pet Breed:</label>
+                    <input type="text" name="petbreed" id="petbreed" required>
+                </div>
+
+                <input type="submit" name="addPet" value="Add Pet" class="btn btn-success mt-3">
+            </form>
         </div>
 
-        <div>
-            <label for="petsex">Pet Sex:</label>
-            <input type="text" name="petsex" id="petsex" required>
+        <div class="container text-center mt-3">
+            <a href="edit-petprofile.php" class="btn btn-lg btn-primary px-3">Back to Pet Profile</a>
         </div>
-
-        <div>
-            <label for="petbreed">Pet Breed:</label>
-            <input type="text" name="petbreed" id="petbreed" required>
-        </div>
-
-        <input type="submit" name="addPet" value="Add Pet">
-        <a href="edit-petprofile.php" class="btn btn-primary">Back to Pet Profile</a>
-    </form>
-
+    </body>
     <!-- Carousel End -->
-    
+
     <?php
     include 'footer.php';
     ?>
