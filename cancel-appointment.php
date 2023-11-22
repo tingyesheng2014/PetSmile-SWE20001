b04ce0fe0abdeebd85110ade66ec0eb792b28ff9
tpk1120 <145960225+tpk1120@users.noreply.github.com>
2023-11-22 10:57:27 +0800

    Update cancel-appointment.php

--- cancel-appointment.php
+++ cancel-appointment.php
@@ -5,9 +5,9 @@ if (isset($_GET['appointmentID']) && isset($_GET['appointmentType'])) {
     $appointmentID = $_GET['appointmentID'];
     $appointmentType = $_GET['appointmentType'];
 
-    if ($appointmentType === "Pet Grooming" || $appointmentType === "Pet Boarding") {
-        $tableName = ($appointmentType === "Pet Grooming") ? "groomingAppt" : "boardingAppt";
-        $primaryKeyColumn = ($appointmentType === "Pet Grooming") ? "GAppt_ID" : "BAppt_ID";
+    if ($appointmentType === "Pet Grooming" || $appointmentType === "Pet Boarding" || $appointmentType === "Pet Treatment") {
+        $tableName = ($appointmentType === "Pet Grooming") ? "groomingAppt" : (($appointmentType === "Pet Boarding") ? "boardingAppt" : "treatmentAppt");
+        $primaryKeyColumn = ($appointmentType === "Pet Grooming") ? "GAppt_ID" : (($appointmentType === "Pet Boarding") ? "BAppt_ID" : "TAppt_ID");
 
         $cancelAppointmentQuery = "UPDATE $tableName SET Status = 'Canceled' WHERE $primaryKeyColumn = '$appointmentID'";
         $cancelAppointmentResult = mysqli_query($con, $cancelAppointmentQuery);
